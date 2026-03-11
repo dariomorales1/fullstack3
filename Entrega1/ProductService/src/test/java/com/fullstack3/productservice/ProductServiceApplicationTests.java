64d8717482695f2d7b20ad52bffd6de2fa07dc79
Dario <dar.moralesa@duocuc.cl>
2026-03-11 19:57:38 -0300

    Creacion del proyecto de la entrega 1

---
+++ Entrega1/ProductService/src/test/java/com/fullstack3/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack3.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Entrega1/src/test/java/com/fullstack3/entrega1/Entrega1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack3.entrega1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entrega1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
